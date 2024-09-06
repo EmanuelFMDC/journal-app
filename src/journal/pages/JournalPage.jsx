313120c0e529ca498f87c23243a3e6c8d3b38646
Emanuel FMDC <emanuel2303e@gmail.com>
2024-09-06 12:24:10 -0600

    target view

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,12 @@
 import { JournalLayout } from "../layout/JournalLayout";
-import { NothingSelectedView } from "../views";
+import { NoteView, NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* Vista que se muestra cuando no hay nada seleccionado */}
-      <NothingSelectedView />
+      {/* <NothingSelectedView /> */}
+      <NoteView/>
     </JournalLayout>
   )
 };
