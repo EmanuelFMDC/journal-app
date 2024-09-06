78b16a53b0aec2bbef11518aec0950d9ddf3df2a
Emanuel FMDC <emanuel2303e@gmail.com>
2024-09-06 12:35:08 -0600

    IconButton

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,7 @@
+import { IconButton } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView, NothingSelectedView } from "../views";
+import { AddOutlined } from "@mui/icons-material";
 
 export const JournalPage = () => {
   return (
@@ -7,6 +9,21 @@ export const JournalPage = () => {
       {/* Vista que se muestra cuando no hay nada seleccionado */}
       {/* <NothingSelectedView /> */}
       <NoteView/>
+
+      <IconButton
+      size="large"
+      sx={{ 
+        color: 'white', 
+        backgroundColor: 'error.main', 
+        ':hover': {backgroundColor: 'error.main', opacity: 0.9}, 
+        position: 'fixed',
+        right: 50,
+        bottom: 50
+      }}
+      >
+        <AddOutlined sx={{ fontSize: 30}}/>
+
+      </IconButton>
     </JournalLayout>
   )
 };
