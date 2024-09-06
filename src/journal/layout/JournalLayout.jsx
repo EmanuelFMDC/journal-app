29e3a0864d81d4e36c0ed9961e9f12095bc2e0ec
Emanuel FMDC <emanuel2303e@gmail.com>
2024-09-06 12:00:23 -0600

    Desing principal view

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,22 +1,27 @@
-import { Box } from "@mui/material"
+import { Box, Toolbar } from "@mui/material"
+import { NavBar, SideBar } from "../components";
 
 const drawerWidth = 240;
 
-export const JournalLayout = ({ Children }) => {
+export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: 'flex'}}>
 
         {/* NAVBAR drawerWidth*/}
-
+        <NavBar drawerWidth={ drawerWidth }/>
         {/* SIDEBAR drawerWidth*/}
+        <SideBar drawerWidth={ drawerWidth }/>
 
         <Box 
         component='main'
         sx={{ flexGrow: 1, p:3}}
         >
             {/* TOOLBAR */}
+            <Toolbar>
+                
+            </Toolbar>
 
-            { Children }
+            { children }
         </Box>
     </Box>
   )
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,11 @@
-import { MailOutline } from "@mui/icons-material"
-import { Typography } from "@mui/material"
-import { JournalLayout } from "../layout/JournalLayout"
+import { JournalLayout } from "../layout/JournalLayout";
+import { NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>Anim aliqua veniam dolor sit id deserunt. Ex irure commodo minim irure Lorem velit dolor eu proident veniam sit. Tempor ex cillum anim excepteur elit laborum adipisicing incididunt tempor. Aliquip est velit cillum duis tempor dolore ad consectetur pariatur ad. Irure in eu et quis elit.</Typography>
-      
+      {/* Vista que se muestra cuando no hay nada seleccionado */}
+      <NothingSelectedView />
     </JournalLayout>
   )
-}
+};
